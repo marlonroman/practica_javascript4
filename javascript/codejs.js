3ba63fbb0c94f3484aee2ef1ca16cc6c7d8ce51d
Marlon Andrés Román Ríos <marlon210499@gmail.com>
2024-07-14 16:02:03 -0500

    agregando suma

---
+++ javascript/codejs.js
@@ -0,0 +1,19 @@
+const num1 = document.getElementById("numero1").value;
+const num2 = document.getElementById("numero2").value;
+
+function sumar(){
+    const num1 = document.getElementById("numero1").value;
+    const num2 = document.getElementById("numero2").value;
+    
+    numero1 = Number(num1);
+    numero2 = Number(num2);
+    console.log(numero1);
+    sumar = numero1 + numero2;
+    document.getElementById("mostrar").textContent = sumar
+}
+
+document.getElementById("restar").onclick = function(){
+    num1 = document.getElement
+}
+
+
